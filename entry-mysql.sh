123b409c7382a9d377ed4012533b53cb52602c70
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2018-03-23 16:16:22 +0200

    Added timeout for registering MySQL.

--- entry-mysql.sh
+++ entry-mysql.sh
@@ -5,6 +5,7 @@ mysql -e 'create database sbtest;'
 sysbench /usr/share/sysbench/tests/include/oltp_legacy/oltp.lua --mysql-table-engine=innodb --oltp-table-size=1000000 --mysql-user=root --db-driver=mysql --rate=5 prepare
 
 pmm-admin config --server pmm-server
+sleep 20
 pmm-admin add mysql MySQL57 --user=root --socket /var/run/mysqld/mysqld.sock --query-source perfschema
 
 while true
